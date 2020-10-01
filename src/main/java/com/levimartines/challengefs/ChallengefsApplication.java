7833b4307e7678ea27adebbdfaa8af93173c5671
Levi Martines <levimartines>
2020-10-01 14:56:09 -0300

    Implementação de Profiles e TestConfig

--- src/main/java/com/levimartines/challengefs/ChallengefsApplication.java
+++ src/main/java/com/levimartines/challengefs/ChallengefsApplication.java
@@ -1,23 +1,12 @@
 package com.levimartines.challengefs;
 
-import com.levimartines.challengefs.service.DbService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChallengefsApplication implements CommandLineRunner {
+public class ChallengefsApplication {
 
-	@Autowired
-	private DbService dbService;
-
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		SpringApplication.run(ChallengefsApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		dbService.instantiateTestDatabase();
 	}
 }
---
+++ src/main/java/com/levimartines/challengefs/config/TestConfig.java
@@ -0,0 +1,27 @@
+package com.levimartines.challengefs.config;
+
+import com.levimartines.challengefs.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DbService dbService;
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() {
+		if (!strategy.equals("create")) {
+			return false;
+		}
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
